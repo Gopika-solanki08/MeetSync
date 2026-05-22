cbb7c86820a9bbb31da2ad8610dc6632426250b4
Gopika Solanki <gopikasolanki6@gmail.com>
2026-05-22 19:00:54 +0530

    Final production cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,10 +11,14 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
-app.use(cors());
+
+app.use(cors({
+  origin: "https://meet-sync-taupe.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
-
 app.use("/api/v1/users", userRoutes);
 
 app.get("/home", (req, res) => {
